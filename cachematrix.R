128d7bbc97bb4ecf93e69b5e0df1e16f9d8b7cd6
Diego Sanz <dfsanz@gmail.com>
2014-10-26 15:58:21 -0400

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,31 @@
-## To test the functions below, first create an invertible matrix and call the makeCacheMatrix function
-## The first time the makeCacheMatrix function is called, since the results haven't yet been cached,
+## First, create an invertible matrix and call the makeCacheMatrix function
+## The first time the makeCacheMatrix function is called, since the results haven't been cached,
 ## the inverse of the matrix will be computed and stored in cache memory
 ## Any subsequent calls to the makeCacheMatrix function from the cacheSolve function 
 ## will return the cached version of the inverted matrix saving computation time in the process
 
-
+## The makeCacheMatrix function declares a number of getter and setter functions
+## The functions are described in-line below
 
 makeCacheMatrix <- function(x = matrix()) {
     
-    # Assign the matrix to ve inverted to the x cache memory variable and set s to NULL
+    # 1. Assign the matrix to ve inverted to the x cache memory variable and set s to NULL
     s <- NULL
     set <- function(y) {
         x <<- y
         s <<- NULL
     }
     
-    # Get the cached value of x
+    # 2. Get the cached value of x
     get <- function() x
     
-    # Set the inverted matrix output for cached variable x to cached variable s
+    # 3. Sets the output for inverting matrix x to cached variable s
     setsolve <- function(solve) s <<- solve
     
-    # Get the inverted matrix stored in cached variable s
+    # 4. Gets the value of s which is either NULL or the cached value of the inverted matrix
     getsolve <- function() s
    
-    # Create list containing "getter" and "setter" functions
+    # 5. Creates list containing "getter" and "setter" functions
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
@@ -33,7 +34,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 # The cacheSolve function checks if the inverse of matrix x
-# has been computed and stored in cache memory. If the inverse has already
+# has been computed and stored in cache memory. If the inverse
 # has not yet been computed, it sets the value in cache via
 # setsolve function.  If the inverse for x already exists in memory
 # then it shows the message "getting cached data" and returns the 
